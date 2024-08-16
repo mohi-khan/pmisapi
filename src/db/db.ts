475c6664295a0b4805fceabb51049ef8dcf49ce4
mohi-khan <mohi.khan@gmail.com>
2024-08-16 19:47:08 +0600

    update db.ts to add ssl certification

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,10 +3,15 @@ import {drizzle} from  "drizzle-orm/node-postgres";
 import dotenv from 'dotenv';
 dotenv.config();
 //console.log(process.env.DB_USER);
+console.log(process.env.DATABASE_URL);
 const pool = new pg.Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port:   +(process.env.DB_PORT ?? 3000)})
+ host:process.env.HOST,
+ database:process.env.DATABASE,
+ user:process.env.USER,
+ password:process.env.PASSWORD,
+port:5432,
+ssl:{
+  rejectUnauthorized: false, // Set to true in production with a valid certificate
+},
+ })
   export const db = drizzle(pool);
